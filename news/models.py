d479a8062576d97de49defa192a53f81f1d3d87d
szantilas87 <szantilas@gmail.com>
2020-04-09 16:04:35 +0100

    Basic app set up and models

---
+++ news/models.py
@@ -0,0 +1,72 @@
+from news import db, login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(
+        db.String(20), nullable=False, default='default_profile.png')
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    news_group = db.relationship('News', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+
+class News(db.Model):
+
+    __tablename__ = 'news'
+
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    comments = db.relationship('Comment', backref='news', lazy=True)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Ad Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
+
+
+class Comment(db.Model):
+    news_group = db.relationship(News)
+
+    id = db.Column(db.Integer, primary_key=True)
+    ad_id = db.Column(db.Integer, db.ForeignKey('news.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, text, ad_id):
+        self.text = text
+        self.ad_id = ad_id
+
+    def __repr__(self):
+        return f"Comment Id: {self.id} --- Date: {self.date} --- Text: {self.text}"
