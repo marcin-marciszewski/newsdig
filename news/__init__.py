7ada04828dc66c66a5483a1c961e662c2341c5db
szantilas87 <szantilas@gmail.com>
2020-05-01 14:23:05 +0100

    Login with google

--- news/__init__.py
+++ news/__init__.py
@@ -3,9 +3,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
 
 
 app = Flask(__name__)
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+print(os.environ.get("GOOGLE_CLIENT_ID"))
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
 
 app.config['SECRET_KEY'] = 'mysecret'
 
@@ -30,9 +47,13 @@ login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 ##################################################
 
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
 from news.core.views import core
 from news.users.views import users
 from news.news_group.views import news_group
+
 
 
 
--- news/users/views.py
+++ news/users/views.py
@@ -1,11 +1,75 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from news import db
+from news import db, get_google_provider_cfg, client, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 from news.models import User, News
 from news.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from news.picture_handlers import add_profile_pic
+import requests
+import json
 
 users = Blueprint('users', __name__)
+
+# Login with google
+@users.route("/google_login")
+def google_login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@users.route("/google_login/callback")
+def callback():
+    code = request.args.get("code")
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+        print(userinfo_response.json())
+
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    user = User.query.filter_by(email=users_email).first()
+
+    if user:
+        login_user(user)
+        return redirect(url_for("core.index"))
+    else:
+        return render_template("no_login.html")
+
 
 # register
 @users.route('/register', methods=['GET', 'POST'])
@@ -33,8 +97,10 @@ def login():
     if form.validate_on_submit():
 
         user = User.query.filter_by(email=form.email.data).first()
+        if not user:
+            return render_template("no_login.html")
 
-        if user.check_password(form.password.data) and user is not None:
+        elif user.check_password(form.password.data) and user is not None:
 
             login_user(user)
             flash('Log in Success!')
